322ddf0037b1538584bf4a2334d7db6b23d3497b
Sean Conlon <conlon@berkeley.edu>
2021-03-22 01:05:46 -0700

    Status Update (UNSTABLE)

--- Examples/Run/Reconstruction/Exatrkx/RecGNNTracks.cpp
+++ Examples/Run/Reconstruction/Exatrkx/RecGNNTracks.cpp
@@ -7,34 +7,34 @@
 // file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
 #include "ActsExamples/Detector/IBaseDetector.hpp"
-#ifdef ACTS_PLUGIN_ONNX
-#include "Acts/Plugins/Onnx/MLTrackClassifier.hpp"
-#endif
 #include "ActsExamples/Digitization/DigitizationOptions.hpp"
+
 #include "ActsExamples/Framework/Sequencer.hpp"
 #include "ActsExamples/Framework/WhiteBoard.hpp"
+
 #include "ActsExamples/Geometry/CommonGeometry.hpp"
+
 #include "ActsExamples/Io/Csv/CsvOptionsReader.hpp"
 #include "ActsExamples/Io/Csv/CsvParticleReader.hpp"
 #include "ActsExamples/Io/Csv/CsvSimHitReader.hpp"
-//include "ActsExamples/Io/Performance/CKFPerformanceWriter.hpp"
-//#include "ActsExamples/Io/Performance/TrackFinderPerformanceWriter.hpp"
+#include "ActsExamples/Io/Performance/TrackFinderPerformanceWriter.hpp"
 #include "ActsExamples/Io/Root/RootTrajectoryParametersWriter.hpp"
 #include "ActsExamples/Io/Root/RootTrajectoryStatesWriter.hpp"
-#include "ActsExamples/MagneticField/MagneticFieldOptions.hpp"
-#include "ActsExamples/Options/CommonOptions.hpp"
-//#include "ActsExamples/TrackFinding/SeedingAlgorithm.hpp"
-#include "ActsExamples/TrackFinding/SpacePointMaker.hpp"
 
-//#include "ActsExamples/TrackFinding/TrackFindingAlgorithm.hpp"
+#include "ActsExamples/MagneticField/MagneticFieldOptions.hpp"
+
+#include "ActsExamples/Options/CommonOptions.hpp"
+
+#include "ActsExamples/TrackFinding/SpacePointMaker.hpp"
+#include "ActsExamples/TrackFinding/TrackParamsEstimationAlgorithm.hpp"
+
+#include "ActsExamples/TrackFitting/SurfaceSortingAlgorithm.hpp"
+#include "ActsExamples/TrackFitting/TrackFittingAlgorithm.hpp"
+#include "ActsExamples/TrackFitting/TrackFittingOptions.hpp"
+
 #include "ActsExamples/TrackInferring/TrackInferringAlgorithm.hpp"
-//#include "ActsExamples/TrackFinding/TrackFindingOptions.hpp"
 #include "ActsExamples/TrackInferring/TrackInferringOptions.hpp"
 
-#include "ActsExamples/TrackFinding/TrackParamsEstimationAlgorithm.hpp"
-#include "ActsExamples/TruthTracking/ParticleSmearing.hpp"
-#include "ActsExamples/TruthTracking/TruthSeedSelector.hpp"
-#include "ActsExamples/TruthTracking/TruthTrackFinder.hpp"
 #include "ActsExamples/Utilities/Options.hpp"
 #include "ActsExamples/Utilities/Paths.hpp"
 #include <Acts/Definitions/Units.hpp>
@@ -50,19 +50,6 @@ using namespace ActsExamples;
 using namespace boost::filesystem;
 using namespace std::placeholders;
 
-void addRecGNNOptions(ActsExamples::Options::Description& desc) {
-  using namespace ActsExamples;
-  using boost::program_options::bool_switch;
-  
-  /*
-  auto opt = desc.add_options();
-  opt("ckf-truth-smeared-seeds", bool_switch(),
-      "Use track parameters smeared from truth particles for steering CKF");
-  opt("ckf-truth-estimated-seeds", bool_switch(),
-      "Use track parameters estimated from truth tracks for steering CKF");
-  */
-}
-
 int runRecGNNTracks(int argc, char* argv[],
                     std::shared_ptr<ActsExamples::IBaseDetector> detector) {
   // setup and parse options
@@ -76,7 +63,6 @@ int runRecGNNTracks(int argc, char* argv[],
   detector->addOptions(desc);
   Options::addMagneticFieldOptions(desc);
   Options::addTrackInferringOptions(desc);
-  addRecGNNOptions(desc);
   Options::addDigitizationOptions(desc);
 
   auto vm = Options::parse(desc, argc, argv);
@@ -92,9 +78,10 @@ int runRecGNNTracks(int argc, char* argv[],
   auto outputDir = ensureWritableDirectory(vm["output-dir"].as<std::string>());
   auto rnd = std::make_shared<ActsExamples::RandomNumbers>(
       Options::readRandomNumbersConfig(vm));
-  //bool truthSmearedSeeded = vm["ckf-truth-smeared-seeds"].template as<bool>();
-  //bool truthEstimatedSeeded =
-  //    vm["ckf-truth-estimated-seeds"].template as<bool>();
+  
+  // Read inference options
+  auto moduleName = variables["ml-module-name"].template as<std::string>();
+  auto funcName = variables["ml-function-name"].template as<std::string>(); 
 
   // Setup detector geometry
   auto geometry = Geometry::build(vm, *detector);
@@ -132,6 +119,7 @@ int runRecGNNTracks(int argc, char* argv[],
   // The selected particles
   const auto& inputParticles = particleSelectorCfg.outputParticles;
 
+  
   // Create space points
   SpacePointMaker::Config spCfg;
   spCfg.inputSourceLinks = digiCfg.outputSourceLinks;
@@ -157,80 +145,89 @@ int runRecGNNTracks(int argc, char* argv[],
   };
   sequencer.addAlgorithm(std::make_shared<SpacePointMaker>(spCfg, logLevel));
   
+  
   // Setup the track inferring algorithm with GNN
-  // It takes ...not sure yet...
+  // It takes Spacepoints, python module and function name to be imported
   auto trackInferringCfg = Options::readTrackInferringConfig(vm);
   trackInferringCfg.inputSpacePoints = spCfg.outputSpacePoints;
-  trackInferringCfg.mlModuleName = "inference_fn";
-  trackInferringCfg.mlFuncName = "gnn_track_finding";
+  trackInferringCfg.mlModuleName = moduleName;
+  trackInferringCfg.mlFuncName = funcName;
+  trackInferringCfg.outputProtoTracks = "sortedprototracks";
   trackInferringCfg.inferTracks = TrackInferringAlgorithm::makeTrackInferrerFunction();
   sequencer.addAlgorithm(
     std::make_shared<TrackInferringAlgorithm>(trackInferringCfg, logLevel));
+  
+  
+  // Algorithm estimating track parameter from seed
+  TrackParamsEstimationAlgorithm::Config paramsEstimationCfg;
+  paramsEstimationCfg.inputSeeds = ""; // it will use spacepoints and input proto tracks as inputs.
+  paramsEstimationCfg.inputProtoTracks = inputProtoTracks;
+  paramsEstimationCfg.inputSpacePoints = {
+      spCfg.outputSpacePoints,
+  };
+  paramsEstimationCfg.inputSourceLinks = digiCfg.outputSourceLinks;
+  paramsEstimationCfg.outputTrackParameters = "estimatedparameters";
+  paramsEstimationCfg.outputProtoTracks = "prototracks_estimated";
+  paramsEstimationCfg.trackingGeometry = tGeometry;
+  paramsEstimationCfg.magneticField = magneticField;
+  paramsEstimationCfg.bFieldMin = 0.1_T;
+  paramsEstimationCfg.deltaRMax = 100._mm;
+  paramsEstimationCfg.sigmaLoc0 = 25._um;
+  paramsEstimationCfg.sigmaLoc1 = 100._um;
+  paramsEstimationCfg.sigmaPhi = 0.005_degree;
+  paramsEstimationCfg.sigmaTheta = 0.001_degree;
+  paramsEstimationCfg.sigmaQOverP = 0.1 / 1._GeV;
+  paramsEstimationCfg.sigmaT0 = 1400._s;
+  paramsEstimationCfg.keepOneSeed = true;
+  sequencer.addAlgorithm(std::make_shared<TrackParamsEstimationAlgorithm>(
+      paramsEstimationCfg, logLevel));
 
-  /*
-  // write track states from CFK
-  RootTrajectoryStatesWriter::Config trackStatesWriter;
-  trackStatesWriter.inputTrajectories = tracFindingCfg.outputTrajectories;
-  // @note The full particles collection is used here to avoid lots of warnings
-  // since the unselected CKF track might have a majority particle not in the
-  // filtered particle collection. This could be avoided when a seperate track
-  // selection algorithm is used.
-  trackStatesWriter.inputParticles = particleReader.outputParticles;
-  trackStatesWriter.inputSimHits = simHitReaderCfg.outputSimHits;
-  trackStatesWriter.inputMeasurementParticlesMap =
-      digiCfg.outputMeasurementParticlesMap;
-  trackStatesWriter.inputMeasurementSimHitsMap =
-      digiCfg.outputMeasurementSimHitsMap;
-  trackStatesWriter.outputDir = outputDir;
-  trackStatesWriter.outputFilename = "trackstates_ckf.root";
-  trackStatesWriter.outputTreename = "trackstates_ckf";
-  sequencer.addWriter(std::make_shared<RootTrajectoryStatesWriter>(
-      trackStatesWriter, logLevel));
+  
+  // Track fitting
+  // setup the fitter
+  TrackFittingAlgorithm::Config fitter;
+  fitter.inputMeasurements = digiCfg.outputMeasurements;
+  fitter.inputSourceLinks = digiCfg.outputSourceLinks;
+  fitter.inputProtoTracks = trkFinderCfg.outputProtoTracks;
+  if (dirNav) {
+    fitter.inputProtoTracks = sorterCfg.outputProtoTracks;
+  }
+  fitter.inputInitialTrackParameters = paramsEstimationCfg.outputTrackParameters;
+  fitter.outputTrajectories = "trajectories";
+  fitter.directNavigation = dirNav;
+  fitter.trackingGeometry = tGeometry;
+  fitter.dFit = TrackFittingAlgorithm::makeTrackFitterFunction(magneticField);
+  fitter.fit = TrackFittingAlgorithm::makeTrackFitterFunction(tGeometry,
+                                                              magneticField);
+  sequencer.addAlgorithm(
+      std::make_shared<TrackFittingAlgorithm>(fitter, logLevel));
+  
+  
+  // write out performance
+  // write track finding/seeding performance
+  TrackFinderPerformanceWriter::Config tfPerfCfg;
+  tfPerfCfg.inputProtoTracks = trkFinderCfg.outputProtoTracks;
+  // using selected particles
+  tfPerfCfg.inputParticles = inputParticles;
+  tfPerfCfg.inputMeasurementParticlesMap = digiCfg.outputMeasurementParticlesMap;
+  tfPerfCfg.outputDir = outputDir;
+  tfPerfCfg.outputFilename = "performance_seeding_trees.root";
+  sequencer.addWriter(
+      std::make_shared<TrackFinderPerformanceWriter>(tfPerfCfg, logLevel));
 
-  // write track parameters from CKF
-  RootTrajectoryParametersWriter::Config trackParamsWriter;
-  trackParamsWriter.inputTrajectories = trackFindingCfg.outputTrajectories;
-  // @note The full particles collection is used here to avoid lots of warnings
-  // since the unselected CKF track might have a majority particle not in the
-  // filtered particle collection. Thsi could be avoided when a seperate track
-  // selection algorithm is used.
-  trackParamsWriter.inputParticles = particleReader.outputParticles;
-  trackParamsWriter.inputMeasurementParticlesMap =
-      digiCfg.outputMeasurementParticlesMap;
-  trackParamsWriter.outputDir = outputDir;
-  trackParamsWriter.outputFilename = "trackparams_ckf.root";
-  trackParamsWriter.outputTreename = "trackparams_ckf";
-  sequencer.addWriter(std::make_shared<RootTrajectoryParametersWriter>(
-      trackParamsWriter, logLevel));
-
-  // Write CKF performance data
+  
+  // Write track finding performance data
   CKFPerformanceWriter::Config perfWriterCfg;
   perfWriterCfg.inputParticles = inputParticles;
-  perfWriterCfg.inputTrajectories = trackFindingCfg.outputTrajectories;
+  perfWriterCfg.inputTrajectories = fitter.outputTrajectories;
   perfWriterCfg.inputMeasurementParticlesMap =
       digiCfg.outputMeasurementParticlesMap;
   // The bottom seed on a pixel detector 'eats' one or two measurements?
-  perfWriterCfg.nMeasurementsMin = particleSelectorCfg.nHitsMin - 2;
+  perfWriterCfg.nMeasurementsMin = particleSelectorCfg.nHitsMin;
   perfWriterCfg.outputDir = outputDir;
-#ifdef ACTS_PLUGIN_ONNX
-  // Onnx plugin related options
-  // Path to default demo ML model for track classification
-  path currentFilePath(__FILE__);
-  path parentPath = currentFilePath.parent_path();
-  path demoModelPath =
-      canonical(parentPath / "MLAmbiguityResolutionDemo.onnx").native();
-  // Threshold probability for neural network to classify track as duplicate
-  double decisionThreshProb = 0.5;
-  // Initialize OnnxRuntime plugin
-  Ort::Env env(ORT_LOGGING_LEVEL_WARNING, "MLTrackClassifier");
-  Acts::MLTrackClassifier neuralNetworkClassifier(env, demoModelPath.c_str());
-  perfWriterCfg.duplicatedPredictor =
-      std::bind(&Acts::MLTrackClassifier::isDuplicate, &neuralNetworkClassifier,
-                std::placeholders::_1, decisionThreshProb);
-#endif
   sequencer.addWriter(
       std::make_shared<CKFPerformanceWriter>(perfWriterCfg, logLevel));
-  */
+
 
   return sequencer.run();
 }
